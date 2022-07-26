5feae01e96a32f63d19c97c261b1594fa94ae74f
gurubhaskary <53305914+gurubhaskary@users.noreply.github.com>
2022-07-26 23:21:11 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,75 @@
+// 1. Print all even numbers in first 50 natural numbers
+	// HINT: google how to determine if a number is even or not
+	// for(let i=1;i<=50;i++)
+	// {
+	// 	if(i%2===0)
+	// 	{
+	// 		console.log(i+" is a even number")
+	// 	}
+	// 	else
+	// 	{
+	// 		console.log(i+" is a odd number")
+	// 	}
+	// }
+
+// 2. print all odd numbers in natural numbers from 20 to 50
+	// HINT: google how to determine if a number is odd or not
+	// for(let i=20;i<=50;i++)
+	// {
+	// 	if(i%2!==0)
+	// 	{
+	// 		console.log(i+" is a odd number")
+	// 	}
+	// 	else
+	// 	{
+	// 		console.log(i+" is a even number")
+	// 	}
+	// }
+
+// 3. print all numbers that are divisible by 5
+// 	[12, 45, 67, 89, 90, 34, 35, 55]
+	// HINT: google how to check divisibility by a number
+	// let a = [12, 45, 67, 89, 90, 34, 35, 55]
+	// for(let i=0;i<a.length;i++)
+	// {
+	// 	if(a[i]%5==0)
+	// 	{
+	// 		console.log(a[i]+" is divisible by 5");
+	// 	}
+	// 	else 
+	// 	{
+	// 		console.log(a[i]+" is not divisible by 5")
+	// 	}
+	// }
+
+// 4. Print all numbers of this array
+// 	 [[1,2], [3,4], [5,6], [7,8]]
+	// HINT: use nested loops
+	// let arr=[[1,2], [3,4], [5,6], [7,8]]
+	// for(let i=0;i<arr.length;i++)
+	// {
+	// 	for(let j=0;j<2;j++)
+	// 	{
+	// 		console.log(arr[i][j]);
+	// 	}
+	// }
+    
+// 5. Print all prime numbers occurring in 1st 50 natural numbers
+	// HINT1: google how to check if a number is prime or not
+    // HINT2: use nested loops
+// 	for(let i=0;i<=50;i++)
+// {
+// 	let flag=0;
+// 	for(let j=2;j<i;j++)
+// 	{
+// 		if(i%j===0)
+// 		{
+// 			flag=1;
+// 			break;
+// 		}
+// 	}
+// 	if(i>1&&flag===0)
+// {
+// 	console.log(i);
+// }
+// }
